e37e1281e645d7e0cc29e79e02eee6d067189d77
Erik Turk <erikturk@gmail.com>
2018-12-05 07:11:54 -0500

    day 5

---
+++ 5/5.py
@@ -0,0 +1,37 @@
+file=open("input","r")
+source= file.readlines()
+file.close()
+input=source[0]
+print(len(input))
+
+i = 1
+start=input[0]
+end=""
+while (i <= (len(input)-2)):
+	#print(i,start,input[i])
+	if str.lower(input[i]) == str.lower(start[-1]):
+		if str.islower(start[-1]) and str.isupper(input[i]):
+			#print(start[-1],input[i+1])
+			if len(start) == 1:
+				start = input[i+1]
+				i+=2
+			else:
+				start = start[:-1]
+				i+=1
+	
+		elif str.isupper(start[-1]) and str.islower(input[i]):
+			#print(start[-1],input[i+1])
+			if len(start) == 1:
+				start = input[i+1]
+				i+=2
+			else:
+				start = start[:-1]
+				i+=1
+		else:
+			start = start + input[i]
+			i+=1
+	else:
+		start= start + input[i]	
+		i+=1
+
+print(start,len(start))
