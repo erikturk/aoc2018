c9595b82e69d467eccf104c829e81baf8901b540
Erik Turk <erikturk@gmail.com>
2018-12-09 12:18:00 -0500

    recursive -works for test but not big

---
+++ 8/8.py
@@ -0,0 +1,45 @@
+import sys
+sys.setrecursionlimit(10**6)
+file = open('testinput','r')
+source = file.read()
+file.close()
+
+print(source)
+stringinput = source.rstrip().split(' ')
+input = [int(x) for x in stringinput]
+
+#input = [1, 4, 0,1,9, 2, 3, 4, 5]
+print(input)
+
+def anychildren(start):
+	if input[start] > 0:
+		return True
+	else:
+		return False
+
+def metadatalist(number,start):
+	metalist = list()
+	for i in range(number):
+		metalist.append(input[start])
+		start +=1
+	
+	return metalist,start
+
+def check(start):
+	num_meta = input[start+1]
+	print('number of children: ',input[start],'number of metavariables',num_meta)
+	if not anychildren(start):
+		data,start = metadatalist(num_meta,start+2)
+		metadata.extend(data)
+	else:
+		for i in range(input[start]):
+			start = check(start+2)
+		data, start = metadatalist(num_meta,start)
+		metadata.extend(data)
+	return start
+
+metadata=[]
+
+check(0)
+print(metadata)
+print(sum(metadata))
