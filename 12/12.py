221d35d63a21c8d2aba1b92d94aeb48c85279d08
Erik Turk <erikturk@gmail.com>
2018-12-14 09:11:33 -0500

    day 12 not yet

--- 12/12.py
+++ 12/12.py
@@ -10,33 +10,87 @@ gen.append(source[0].split(" ")[2].rstrip())
 for i in range(2,len(source)):
 	rules.append( source[i].rstrip())
 
+potdict=dict()
 pprint(rules)
 print(gen)
 
-thisgen = gen[0]+'....'
+gen[0] = gen[0]
+print(gen)
+thisgen = gen[0]
 newstart = -2
-while len(gen) <= 20:
+
+def tester(testpot):
+	for rule in rules:
+		condition = rule.split(' ')[0]
+		result = rule.split(' ')[2]
+		if condition == testpot:
+			return result
+	return '.'
+
+def printgen(potdict,start=0):
+	for i in range(min(min(list(potdict.keys())),start),max(potdict.keys())+3):
+		if i in potdict:
+			print(potdict[i],end='')
+		else:
+			print('.',end="")
+	print()
+	return
+		
+### fill a dict with location:status
+
+for i,v in enumerate(list(source[0].split(" ")[2].rstrip())):
+	potdict[i]=v
+potdictgen = list()
+print('start potdict is:')
+printgen(potdict)
+potdictgen.append(potdict)
+
+
+
+while len(potdictgen) <= 20:
+	print('generation is: ',len(potdictgen)-1)
+	printgen(potdict)
+
+	startpotdict =min(potdict.keys()) # for this potdict, this is the start
+	nextpotdict = dict()
+	# let's check the 4 pots to the left of the start
+	addpots = 4
+	for i in range(startpotdict-4,startpotdict):
+		testpot = ('.' * addpots)
+		for j in range (0,5-addpots):
+			testpot = testpot + potdict[j]
+		if tester(testpot) == '#':
+			nextpotdict[i+2]=tester(testpot)
+		addpots -= 1
+
+
+	print(nextpotdict)
 	
 	nextgen = ""
-	for pot in range(len(thisgen)):
-		testpot = thisgen[pot:pot+5]
-		answer = 'X'
-		for rule in rules:
-			condition = rule.split(' ')[0]
-			result = rule.split(' ')[2]
-			if condition == testpot:
-				answer = result
-		if answer == 'X':
-			answer = '.'
-		nextgen = nextgen+answer
-	gen.append(nextgen)	
-	thisgen = '..'+''.join(gen[-1])+'..'
-	newstart += -1
-for i,v  in enumerate(gen):
-	print('%2d:' % i,v)
-total = 0
-for i in range(newstart,len(gen[-1])):
-	if gen[-1][i] == '#':
-		total += i
 
+	
+	
+	for i in range(max(potdict.keys())+4):
+		testpot=""
+		for j in range(i-2,i+3):
+			if j in potdict:
+				testpot = testpot + potdict[j]
+			else:
+				testpot = testpot + '.'
+		nextpotdict[i]=tester(testpot)
+
+	printgen(nextpotdict)
+	potdictgen.append(nextpotdict)
+	potdict = potdictgen[-1]
+	printgen(potdict)
+	nextpotdict = dict()
+
+
+for i,v  in enumerate(potdictgen):
+	print('%2d:' % i,end="")
+	printgen(v,-3)
+total = 0
+for i in potdictgen[-1]:
+	if potdictgen[-1][i]=='#':
+		total += i
 print(total)
