3f18b7f86ac41ad8a1eb78548f4da208ddd94000
Erik Turk <erikt@cent7b.erikturk.com>
2018-12-14 00:07:24 -0500

    some day 12, but not all

--- 12/12.py
+++ 12/12.py
@@ -4,23 +4,39 @@ source = file.readlines()
 file.close()
 
 gen=list()
-rule = list()
+rules = list()
 gen.append(source[0].split(" ")[2].rstrip())
 
 for i in range(2,len(source)):
-	rule.append( source[i])
+	rules.append( source[i].rstrip())
 
-
-
-
-
-
-
-
-
-
-
-
+pprint(rules)
 print(gen)
 
-pprint(rule)
+thisgen = gen[0]+'....'
+newstart = -2
+while len(gen) <= 20:
+	
+	nextgen = ""
+	for pot in range(len(thisgen)):
+		testpot = thisgen[pot:pot+5]
+		answer = 'X'
+		for rule in rules:
+			condition = rule.split(' ')[0]
+			result = rule.split(' ')[2]
+			if condition == testpot:
+				answer = result
+		if answer == 'X':
+			answer = '.'
+		nextgen = nextgen+answer
+	gen.append(nextgen)	
+	thisgen = '..'+''.join(gen[-1])+'..'
+	newstart += -1
+for i,v  in enumerate(gen):
+	print('%2d:' % i,v)
+total = 0
+for i in range(newstart,len(gen[-1])):
+	if gen[-1][i] == '#':
+		total += i
+
+print(total)
